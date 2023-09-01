27f18808ed9e0c9b621f46f9969d8e29c139e738
doyou1 <tla149@daum.net>
2023-09-01 17:20:41 +0900

    chartjs project init

---
+++ vue/inflearn-lv5/vue-chart/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <canvas id="myChart"></canvas>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { Chart, registerables } from "chart.js";
+import { onMounted } from "vue";
+
+Chart.register(...registerables);
+
+onMounted(() => {
+  const ctx = (
+    document.getElementById("myChart") as HTMLCanvasElement
+  ).getContext("2d");
+  if (ctx) {
+    new Chart(ctx, {
+      type: "bar",
+      data: {
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: [
+          {
+            label: "# of Votes",
+            data: [12, 19, 3, 5, 2, 3],
+            borderWidth: 1,
+          },
+        ],
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true,
+          },
+        },
+      },
+    });
+  }
+});
+</script>
